c4e0d7410d1a1e1434d2fa949723e87d5fa33052
eunsaem <honges931108@naver.com>
2018-11-22 15:50:35 +0900

    first commit

---
+++ src/main/java/com/hybrid/hybrid/JsonTestVO.java
@@ -0,0 +1,19 @@
+package com.hybrid.hybrid;
+
+public class JsonTestVO {
+	String name;
+	String id;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+
+}
